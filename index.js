1c4891783bcb436d4f250cfa6fafbe2ad90f84ed
SagnikGhosh18 <sagnikghosh18@gmail.com>
2025-01-08 16:58:57 +0530

    Testing the deployment for routes

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const app = express();
-const PORT = 4000;
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const userRoutes = require('./src/routes/userRoutes');
+const transactionRoutes = require('./src/routes/transactionRoutes');
 
-app.get('/home', (req, res) => {
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/users', userRoutes);
+app.use('/transactions', transactionRoutes);
+
+app.get('/', (req, res) => {
     res.status(200).json('Welcome, your app is working well');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    await mongoose.connect(process.env.MONGO_URI || '');
+    console.log('Connected to MongoDB');
     console.log(`Server running at http://localhost:${PORT}`);
 });
 
